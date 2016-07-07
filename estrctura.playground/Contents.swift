b66048e04f639e5ec244e5597e5fd2faed7c75f2
David Raygoza <david_raygoza@MacBook-Pro-de-David-Raygoza-2.local>
2016-07-07 00:06:14 -0500

    ejercicio con estrutura

---
+++ estrctura.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Automovil{
+    var marca: String
+    var precio: Double
+}
+
+var auto = Automovil(marca: "vocho", precio: 185000.30)
+auto.marca
+auto.precio
+
